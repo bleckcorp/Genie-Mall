b1811f03b1db914ce2624c592610f46ffb8006d3
Blessing Chukwubuogum <59502310+bleckcorp@users.noreply.github.com>
2023-03-20 13:17:29 +0100

    Initial commit

---
+++ src/main/java/com/example/geniemall/GenieMallApplication.java
@@ -0,0 +1,13 @@
+package com.example.geniemall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenieMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenieMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/geniemall/GenieMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.geniemall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenieMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
